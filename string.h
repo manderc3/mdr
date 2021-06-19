df9c0d7dea0799c7cc8d722a2910dc46b684d066
Chris Mander <mandercs3@gmail.com>
2021-06-19 14:04:39 +0100

    added string::substr

--- string.h
+++ string.h
@@ -3,6 +3,8 @@
 
 #include "types.h"
 #include "utility.h"
+#include "assert.h"
+#include "iterator.h"
 
 #include <iostream>
 
@@ -20,6 +22,13 @@ namespace mdr
 	    , m_buffer(internal_alloc(m_capacity))
 	{
 	    copy_elements(data, m_buffer, m_size);
+	}
+
+	string(char* data)
+	    : m_capacity(determine_size(data))
+	    , m_size(m_capacity)
+	    , m_buffer(data)
+	{
 	}
 
 	string(const string& rhs)
@@ -96,11 +105,23 @@ namespace mdr
 	{
 	    return m_buffer;
 	}
+
+	string substr(const size_t pos, const char matcher[]) const
+	{
+	    return internal_substr(pos, matcher);
+	}
+
+	string substr(const size_t pos, const string& matcher) const
+	{
+	    return internal_substr(pos, matcher.data());
+	}
 	
     private:
 	char* internal_alloc(const size_t capacity)
 	{
+	    std::cout << "internal_alloc\n";
 	    return new char[capacity];
+	    std::cout << "leaving internal_alloc\n";
 	}
 
 	void internal_dealloc()
@@ -122,15 +143,46 @@ namespace mdr
 	    rhs.m_buffer = nullptr;
 	}
 
-	size_t determine_size(const char data[])
+	size_t determine_size(const char data[]) const
 	{
+	    std::cout << "oops\n";
 	    size_t result { 0 };
 
 	    for (; data[result] != '\0'; result++)
 	    {
 	    }
+
+	    std::cout << result << '\n';
 	    
 	    return result;
+	}
+
+	string internal_substr(const size_t index, const char matcher[]) const
+	{
+	    assert(index >= 0 && index < m_size, "Index provided is not valid");
+	    assert(matcher != nullptr, "Matcher is nullptr");
+
+	    const size_t matcher_size = determine_size(matcher);
+
+	    assert(index + matcher_size < m_size, "Matcher size relative to index exceeds the size of the character buffer");
+
+	    for (size_t i = index; i < m_size; i++)
+	    {
+		size_t matcher_count { 0 };
+		for(; m_buffer[i + matcher_count] == matcher[matcher_count] && m_buffer[i + matcher_count] != '\0'; ++matcher_count)
+		{
+		}
+
+		if (matcher_count == matcher_size)
+		{
+		    char* foo = new char[matcher_size];
+		    copy_element_range(m_buffer, foo, i, matcher_count);
+
+		    return string(foo);
+		}
+	    }
+
+	    return "";
 	}
 	
 	size_t m_capacity;
